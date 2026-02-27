0fdc409d72658cba55ac557d83d9b35ddc6ebc64
Kyryl Rudyk <kyrylrudyk03@gmail.com>
2026-02-27 19:02:46 +0800

    додав просто число

--- utils.py
+++ utils.py
@@ -12,3 +12,10 @@ def fib(n):
         for _ in range(n):
             a, b = b, a + b
         return a
+
+def is_prime(n):
+    if n < 2: return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
