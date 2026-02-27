5410b3261ef0d72a02d00776335321bd16cfba7a
Nazarchik <sorochenko.n.s@gmail.com>
2026-02-27 13:19:59 +0200

    nsd + fix

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from utils import get_fact, fib
+from utils import get_fact, fib, nsd
 
-x = input()
+x = int(input())
+y = int(input())
 
 print("Factorial:", get_fact(x))
 
 print("Fib:", fib(x))
+
+print("nsd:", nsd(x,y))
+
